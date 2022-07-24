8c15a35789ba310f387e71b1f71baafe9ebded8f
Ludwig <ludwig.moreno@gmail.com>
2022-07-24 17:06:52 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.vue
    	src/components/UsersFile.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div id="app">
+    <UsersVariable />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UsersVariable from './components/UsersFile.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UsersVariable
   }
 }
 </script>
